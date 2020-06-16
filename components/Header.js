9f3ef2e2bf592b345587b2598909dea427b51ede
ghigbie <gehigbie@gmail.com>
2020-06-15 20:07:14 -0700

    Add styles to header component

--- components/Header.js
+++ components/Header.js
@@ -2,9 +2,20 @@ import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
 const Header = () => {
-  return <View style={StyleSheet.container} />;
+  return (
+      <View style={styles.container}>
+        <Text style-{styles.headerText}>Header</Text>
+     </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+      height: '12%'
+  },
+  headerText: {
+      color: '#888'
+  }
 });
+
+export default Header;
