dc90ca0731f21927d12bb8d7090c901f40b62270
ghigbie <gehigbie@gmail.com>
2020-06-15 20:04:30 -0700

    Add component configure eslint

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Header = () => {
+  return <View style={StyleSheet.container} />;
+};
+
+const styles = StyleSheet.create({
+  container: {},
+});
