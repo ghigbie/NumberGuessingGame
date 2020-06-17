71876ea7f2c98a3657f69aba3c231904334cf612
ghigbie <gehigbie@gmail.com>
2020-06-16 19:25:45 -0700

    Edit styles

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,20 +5,23 @@ const StartGameScreen = ({}) => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Select a Number</Text>
-      <TextInput />
-      <View style={styles.buttonContainer}>
-        <Button
-          title="Reset"
-          onPress={() => {
-            console.log('Reset was pressed');
-          }}
-        />
-        <Button
-          title="Confirm"
-          onPress={() => {
-            console.log('Confirm was pressed');
-          }}
-        />
+      <View style={styles.inputContainer}>
+        <TextInput />
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Reset"
+            color="#f7287b"
+            onPress={() => {
+              console.log('Reset was pressed');
+            }}
+          />
+          <Button
+            title="Confirm"
+            onPress={() => {
+              console.log('Confirm was pressed');
+            }}
+          />
+        </View>
       </View>
     </View>
   );
@@ -31,8 +34,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-start',
   },
-  screenText: {
+  title: {
     color: '#888',
+    fontSize: 20,
+    marginVertical: 12,
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: '80%',
+    alignItems: 'center',
+    shadowColor: '#888',
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    backgroundColor: '#fff',
+    elevation: 5,
+    padding: 24,
+    borderRadius: 12,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    paddingHorizontal: 14,
   },
 });
 
