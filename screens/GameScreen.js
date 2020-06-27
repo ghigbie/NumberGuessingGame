c8520ea2a58829d4a72acc127ed90fe7c167d660
ghigbie <gehigbie@gmail.com>
2020-06-27 15:42:05 -0700

    Add more buttons

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,11 +1,44 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, Button, StyleSheet} from 'react-native';
 import Colors from './../constants/colors';
 
-const GameScreen = () => {
+import NumberContainer from './../components/NumberContainer';
+import Card from './../components/Card';
+
+const generateRandomBetween = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const randomNumber = Math.floor(Math.random() * (max - min)) + min;
+  if (randomNumber === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return randomNumber;
+  }
+};
+
+const GameScreen = ({userChoice}) => {
+  const [currentGuess, setCurrentGuess] = useState(
+    generateRandomBetween(1, 100),
+    userChoice,
+  );
   return (
     <View style={styles.container}>
-      <Text>Game Screen</Text>
+      <Text>Opponent's Guess</Text>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card>
+        <Button
+          title="Lower"
+          onPress={() => {
+            console.log('lower pressed');
+          }}
+        />
+        <Button
+          title="Greater"
+          onPress={() => {
+            console.log('greater pressed');
+          }}
+        />
+      </Card>
     </View>
   );
 };
