15028c20500d66fa9e0d173cfb2ef568e6613dbd
ghigbie <gehigbie@gmail.com>
2020-07-07 19:26:33 -0700

    Edit componennts

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,57 +1,76 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button, Image, Dimensions} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Dimensions,
+  ScrollView,
+} from 'react-native';
 import Colors from './../constants/colors';
 import TitleText from './../components/TitleText';
 import BodyText from './../components/BodyText';
 import MainButton from './../components/MainButton';
-import colors from './../constants/colors';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 const GameOverScreen = ({guessRounds, userNumber, onStartGame}) => {
   return (
     <View style={styles.container}>
-      <TitleText style={styles.text}>Game Over!</TitleText>
-      <Image
-        source={require('./../assets/success.png')}
-        style={styles.image}
-        resizeMode="cover"
-      />
-      <BodyText style={styles.text}>
-        I won! It only took me{' '}
-        <Text style={styles.highlight}>{guessRounds}</Text>
-        {guessRounds > 1 ? ' tries' : ' try'}!
-      </BodyText>
-      <Text style={styles.text}>
-        The number was: <Text style={styles.highlight}>{userNumber}</Text>
-      </Text>
-      <View style={styles.buttonContainer}>
-        <MainButton onPress={onStartGame}>
-          <Icon name="rocket" size={24} color="#fff" />
-        </MainButton>
-      </View>
+      <ScrollView
+        contentContainerStyles={styles.scrollView}
+        showsVerticalScrollIndicator={true}>
+        <View style={styles.centerContainer}>
+          <TitleText style={styles.text}>Game Over!</TitleText>
+          <Image
+            source={require('./../assets/success.png')}
+            style={styles.image}
+            resizeMode="cover"
+          />
+          <BodyText style={styles.text}>
+            I won! It only took me{' '}
+            <Text style={styles.highlight}>{guessRounds}</Text>
+            {guessRounds > 1 ? ' tries' : ' try'}!
+          </BodyText>
+          <Text style={styles.text}>
+            The number was: <Text style={styles.highlight}>{userNumber}</Text>
+          </Text>
+          <View style={styles.buttonContainer}>
+            <MainButton onPress={onStartGame}>
+              <Icon name="rocket" size={24} color="#fff" />
+            </MainButton>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
+    height: windowHeight,
     alignItems: 'center',
-    margin: 8,
-    marginVertical: 124,
-    minHeight: 300,
+    marginHorizontal: 8,
+    marginVertical: windowHeight * 0.0175,
+  },
+  scrollView: {
+    flex: 1,
+  },
+  centerContainer: {
+    alignItems: 'center',
   },
   text: {
-    fontSize: 24,
+    fontSize: windowHeight < 400 ? 16 : 20,
     height: 28,
     marginVertical: 14,
     textAlign: 'center',
   },
   buttonContainer: {
-    marginVertical: 24,
+    marginVertical: windowHeight * 0.025,
+    width: 100,
+    alignItems: 'center',
   },
   image: {
     width: windowWidth * 0.7,
@@ -59,7 +78,7 @@ const styles = StyleSheet.create({
     borderRadius: (windowWidth * 0.7) / 2,
   },
   highlight: {
-    color: colors.primary,
+    color: Colors.primary,
     fontFamily: 'OpenSans-Bold',
   },
 });
