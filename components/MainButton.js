2033be3491329a377e299be120b20b4259ce0e26
ghigbie <gehigbie@gmail.com>
2020-08-08 20:28:12 -0700

    Add more platform styles

--- components/MainButton.js
+++ components/MainButton.js
@@ -1,17 +1,37 @@
 import React from 'react';
 
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Platform,
+} from 'react-native';
 import Colors from './../constants/colors';
 
-const MainButton = ({style, onPress, children}) => (
-  <TouchableOpacity activeOpacity={0.8} onPress={onPress}>
-    <View style={{...styles.container, ...style}}>
-      <Text style={styles.text}>{children}</Text>
+const MainButton = ({style, onPress, children}) => {
+  let ButtonComponent = TouchableOpacity;
+  if (Platform.OS === 'android' && Platform.Version >= 21) {
+    ButtonComponent = TouchableNativeFeedback;
+  }
+
+  return (
+    <View style={styles.wrapper}>
+      <ButtonComponent activeOpacity={0.8} onPress={onPress}>
+        <View style={{...styles.container, ...style}}>
+          <Text style={styles.text}>{children}</Text>
+        </View>
+      </ButtonComponent>
     </View>
-  </TouchableOpacity>
-);
+  );
+};
 
 const styles = StyleSheet.create({
+  wrapper: {
+    borderRadius: 25,
+    overflow: 'hidden',
+  },
   container: {
     backgroundColor: Colors.primary,
     paddingVertical: 8,
